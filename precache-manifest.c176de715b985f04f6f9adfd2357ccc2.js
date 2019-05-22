1ccc5c41fd317ede63a589855c3840710c1a8050
Miguel Martinez <miguel.martinez11@gmail.com>
2019-05-22 10:40:48 +0200

    Updates

---
+++ precache-manifest.c176de715b985f04f6f9adfd2357ccc2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecb362e8c07670d71a2e299c68332bac",
+    "url": "/meteor/index.html"
+  },
+  {
+    "revision": "a8ba2ecce9ae0c376cfb",
+    "url": "/meteor/static/js/2.730dc6c3.chunk.js"
+  },
+  {
+    "revision": "6435a27ddf9d1819c4ba",
+    "url": "/meteor/static/js/main.69fdd740.chunk.js"
+  },
+  {
+    "revision": "1bd1d9e42a990a3a361f",
+    "url": "/meteor/static/js/runtime~main.c3248b4c.js"
+  }
+]);
